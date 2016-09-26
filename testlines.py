88e394187dd63ae2201376fb78c956f8cfdf0a54
osamamhdl <osamamhdl@outlook.com>
2016-09-26 21:45:32 +0300

    all work.

--- testlines.py
+++ testlines.py
@@ -9,11 +9,11 @@
 # Description			: This very simple script open a file and prints out 100 lines of whatever is set for the line variableest you want to print\n"	# This sets the variable for the text that you want to print
 
 
-def Write_to_file(filename,txt):	 # Sorry this is just for test.
+def write_to_file(filename,txt):	 # Sorry this is just for test.
   with open(filename,'w') as file_object:
       s = file_object.write(txt)
       
     
 if __name__ == '__main__':
-    Write_to_file('test.txt', 'i am beven')		#Sorry this is just for test.
+    write_to_file('test.txt', 'i am beven')		#Sorry this is just for test.
 
